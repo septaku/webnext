c0474ee904595f5740cdf0b822e150ee6c35b47b
septaku <septakusumautama@gmail.com>
2023-10-23 14:14:35 +0700

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme } from '@mantine/core';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light',
+    }}
+    >
+      <Component{...pageProps}/>
+    </MantineProvider>
+  );
 }
